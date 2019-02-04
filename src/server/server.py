cdfdc02a36e3a743aac8c80517060983fbb5cf82
Phie <phie@phie.ovh>
2019-02-04 20:18:26 +0100

    prepare + get keyword db

--- src/server/server.py
+++ src/server/server.py
@@ -17,23 +17,41 @@ class myHandler(BaseHTTPRequestHandler):
     APP_PATH = "/home/phieelementary/Dev/GitBis/QuickDoc/CarnetNextcloud/templates/CarnetElectron"
 	#Handler for the GET requests
     def do_GET(self):
+        try:
+            index = self.path.index("?")
+            spath = self.path[0:index]
+            params = parse_qs(self.path[index+1:])
+        except ValueError:
+            print ("? not found")
+            spath = self.path
         if(self.path.startswith("/api/")):
-            spathCmp = self.path[len("/api/"):]
-            try:
-                index = spathCmp.index("?")
-                spath = spathCmp[0:index]
-                params = parse_qs(spathCmp[index+1:])
-            except ValueError:
-                print ("? not found")
-                spath = spathCmp
+            spath = spath[len("/api/"):]
+
             print(spath)
             if spath == "recentdb":
-                file = open(self.settingsManager.getNotePath()+"/quickdoc/recentdb/"+self.settingsManager.getUUID(), 'r')
-                text = file.read()
-                file.close()
+                try:
+                    file = open(self.settingsManager.getNotePath()+"/quickdoc/recentdb/"+self.settingsManager.getUUID(), 'r')
+                    text = file.read()
+                    file.close()
+                except FileNotFoundError:
+                    text = "{\"data\":[]}"
                 data = bytes(text, "utf8")
                 self.send_response(200)
                 self.send_header('Content-type','application/json')
+            elif spath == "keywordsdb":
+
+                try:
+                    file = open(self.settingsManager.getNotePath()+"/quickdoc/keywords/"+self.settingsManager.getUUID(), 'r')
+                    text = file.read()
+                    file.close()
+                except FileNotFoundError:
+                    text = "{\"data\":[]}"
+                data = bytes(text, "utf8")
+                self.send_response(200)
+                self.send_header('Content-type','application/json')
+            elif spath == "note/open/prepare":
+                data = bytes("/reader/reader.html", "utf8")
+                self.send_response(200)
             elif spath == "browser/list":
                 files = os.listdir(self.settingsManager.getNotePath()+"/"+params['path'][0])
                 ret = []
