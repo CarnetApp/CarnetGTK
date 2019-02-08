3467ddf0850e5d2cef5a6528bda19691226e3975
Phie <phie@phie.ovh>
2019-02-08 03:02:04 +0100

    gtk toolbar for editor

--- src/window.py
+++ src/window.py
@@ -26,15 +26,70 @@ class CarnetgtkWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'CarnetgtkWindow'
 
     webview = GtkTemplate.Child()
-
+    header_bar = GtkTemplate.Child()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        import os
-        print(os.path.dirname(os.path.realpath(__file__)))
 
         WebKit2.WebView()
         self.init_template()
         #self.webview.load_uri("file:///home/phieelementary/Dev/GitBis/QuickDoc/CarnetNextcloud/templates/CarnetElectron/index.html")
-
+        self.webview.connect("notify::title", self.window_title_change) #only way to receive messages...
         self.webview.load_uri("http://localhost:8087")
+        self.switch_to_browser()
         #self.webview.get_inspector().detach()
+
+
+    def on_format_clicked(self, view):
+        print("on format clicked")
+        self.toggle_toolbar("format-toolbar")
+
+    def on_edit_clicked(self, view):
+        print("on format clicked")
+        self.toggle_toolbar("edit-toolbar")
+
+    def on_media_clicked(self, view):
+        print("on format clicked")
+        self.toggle_toolbar("media-toolbar")
+
+    def on_tools_clicked(self, view):
+        print("on format clicked")
+        self.toggle_toolbar("tools-toolbar")
+
+    def toggle_toolbar(self, toolbar):
+        self.webview.run_javascript("writerFrame.contentWindow.writer.toolbarManager.toggleToolbar(writerFrame.contentWindow.document.getElementById('"+toolbar+"'))")
+
+    def window_title_change(self, v, param):
+        print("on title changed")
+        if not v.get_title():
+            return
+        if v.get_title().startswith("msgtopython:::"):
+            message = v.get_title().split(":::",1)[1]
+            # Now, send a message back to JavaScript
+            if(message == "noteloaded"):
+                self.switch_to_editor()
+            elif(message == "exit"):
+                self.switch_to_browser()
+
+    def switch_to_editor(self):
+        for child in self.header_bar.get_children():
+            if(child.get_name() == "new-note"):
+                child.hide()
+            elif(child.get_name() == "new-folder"):
+                child.hide()
+        for child in self.header_bar.get_custom_title().get_children():
+            if(child.get_name() == "title-label"):
+                child.hide()
+            elif(child.get_name() == "editor-box"):
+                child.show()
+    def switch_to_browser(self):
+        for child in self.header_bar.get_children():
+            if(child.get_name() == "new-note"):
+                child.show()
+            elif(child.get_name() == "new-folder"):
+                child.show()
+        for child in self.header_bar.get_custom_title().get_children():
+            if(child.get_name() == "title-label"):
+                child.show()
+            elif(child.get_name() == "editor-box"):
+                child.hide()
+
