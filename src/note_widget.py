3243e3d08d5219e3d7505662bb5b1538d89ec2dd
Phie <phie@phie.ovh>
2019-10-11 19:07:08 +0200

    open editor on note click

--- src/note_widget.py
+++ src/note_widget.py
@@ -20,17 +20,18 @@ gi.require_version('WebKit2', '4.0')
 from gi.repository import WebKit2
 from .gi_composites import GtkTemplate
 from .settings_manager import *
-
+from os import path
 @GtkTemplate(ui='/org/gnome/Carnetgtk/note_widget.ui')
-class NoteWidget(Gtk.Box):
+class NoteWidget(Gtk.EventBox):
     __gtype_name__ = 'NoteWidget'
     title_label = GtkTemplate.Child()
     text_label = GtkTemplate.Child()
     def __init__(self, note):
         super().__init__()
         self.init_template()
+        self.note = note
         self.get_style_context().add_class("note")
         self.text_label.set_text(note["shorttext"])
         self.text_label.set_line_wrap(True)
-        self.title_label.set_text(note['title'])
-
+        self.title_label.set_text(path.basename(note['path']))
+        self.title_label.set_line_wrap(True)
--- src/ui/note_list.py
+++ src/ui/note_list.py
@@ -1,27 +1,41 @@
 from .note_manager import NoteManager
 from .settings_manager import *
 from .note_widget import *
-
+import os, sys
+from stat import *
 class NoteList:
     def __init__(self, container, scrollview):
         self.container = container
+
         self.scrollview = scrollview
         self.objects = self.get_objects()
         row = 0
         while( row != len(self.objects)):
-            noteManager = NoteManager(settingsManager.getNotePath()+"/"+self.objects[row])
-            notear = {"shorttext":noteManager.getMetadata()['shorttext'], "title":"title "+str(row)}
-            note = NoteWidget(notear)
-            note.show_all()
-            note.modify_bg(Gtk.StateFlags.NORMAL, Gdk.color_parse('white'))
-            self.container.add_child(note)
+            path = self.objects[row]
+            s = os.stat(settingsManager.getNotePath()+"/"+path)
+            if(S_ISDIR(s.st_mode)):
+                row = row + 1
+                continue
+            noteManager = NoteManager(settingsManager.getNotePath()+"/"+path)
+            note = noteManager.getMetadata()
+            note['path'] = path
+            note_widget = NoteWidget(note)
+            note_widget.show_all()
+            note_widget.modify_bg(Gtk.StateFlags.NORMAL, Gdk.color_parse('white'))
+            note_widget.connect("button-release-event", self.on_note_clicked)
+            self.container.add_child(note_widget)
             row = row+1
 
 
         self.scrollview.get_vadjustment().connect("value-changed",self.onChanged)
         self.container.show_all()
 
+    def set_window(self, window):
+        self.window = window
 
+    def on_note_clicked(self, note_widget, bla):
+        print("clicked ! "+note_widget.note['path'])
+        self.window.switch_to_editor(note_widget.note['path'])
     def get_objects(self):
         return False
 
